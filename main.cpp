02214eeda419a72c1aa2094ee852e08a889deb04
tuyenlt <keplerlu@gmail.com>
2024-07-21 22:43:51 +0700

    test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 #include<sys/time.h>
-#include<cstdlib>
+
 
 using namespace std;
  
@@ -8,7 +8,7 @@ using namespace std;
 #define int long long
 const int MOD = 1e9 + 7;
 const int mxn = 2e5 + 1;
-
+// #define ONLINE_JUDGE "off"
 
 //
 //.
@@ -19,58 +19,57 @@ const int mxn = 2e5 + 1;
 //
 
 
-const int SIZE = 1000;
-
 
 void pre_process(){
-    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 }
 
-
-long long min_energy_to_reach_end(int N, int C, const vector<int>& H) {
-    vector<long long> dp(N, LLONG_MAX);
-    dp[0] = 0;
-
-    deque<int> dq;
-    dq.push_back(0);
-
-    for (int i = 1; i < N; ++i) {
-
-        while (dq.size() > 1 && dp[dq[1]] + (long long)(H[i] - H[dq[1]]) * (H[i] - H[dq[1]]) <= dp[dq[0]] + (long long)(H[i] - H[dq[0]]) * (H[i] - H[dq[0]])) {
-            dq.pop_front();
-        }
-        
-        dp[i] = dp[dq.front()] + (long long)(H[i] - H[dq.front()]) * (H[i] - H[dq.front()]) + C;
-
-        while (dq.size() > 1 && dp[i] + (long long)(H[i] - H[dq.back()]) * (H[i] - H[dq.back()]) <= dp[dq.back()] + (long long)(H[dq.back()] - H[dq[dq.size() - 2]]) * (H[dq.back()] - H[dq[dq.size() - 2]])) {
-            dq.pop_back();
-        }
-
-        dq.push_back(i);
-    }
-
-    return dp[N - 1];
-}
-
-void solve(){
+void test_answer(){
     int n,c;
     cin >> n >> c;
-    vector<int> h(n);
-    // h[0] = 1;
-    // for(int i=1;i<n;i++){
-    //     h[i] = h[i-1] + (rand() % 10) + 1;
-    // }
+    int a[n];
     for(int i=0;i<n;i++){
-        cin >> h[i];
+        cin >> a[i];
     }
-    cout << min_energy_to_reach_end(n,c,h);
+    int dp[n];
+    for(int i=0;i<n;i++){
+        dp[i] = LONG_LONG_MAX;
+    }
+    dp[0] = 0;
+    int pre[n];
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            int jump_cost = (a[j] - a[i])*(a[j] - a[i]) + c;
+            if(jump_cost + dp[i] < dp[j]){
+                dp[j] = jump_cost + dp[i];
+                pre[j] = i;
+            }
+        }
+    }
+    cout << dp[n-1] << endl;
+    int tmp = n-1;
+    while (pre[tmp] != 0)
+    {   
+        cout << tmp << " ";
+        tmp = pre[tmp];
+        /* code */
+    }
+    cout << 0 << endl;
+    
+}
+
+
+void solve(){
+    test_answer();
 }
 
 
  
 signed main(){
+    
 #ifndef ONLINE_JUDGE    
     freopen("/home/tuyenlt/C++/Algorthims/input.txt", "r", stdin);
+    // freopen("/home/tuyenlt/C++/Algorthims/gen_input.txt", "r", stdin);
     freopen("/home/tuyenlt/C++/Algorthims/output.txt","w",stdout);
      struct timeval start, end;
     gettimeofday(&start, NULL);
